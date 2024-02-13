e885601974c4bf015d28d93284aa377134f2e735
muse9312 <muse9312@naver.com>
2024-02-13 12:55:06 +0900

    fix : Router apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import "./App.css";
-// import "./styles/reset.css";
-// import "./styles/page.css";
+import "./styles/reset.scss";
+import "./styles/page.scss";
+import { RouterProvider, Routers, Router } from "react-router-dom";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
 
 function App() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper">
+      <RouterProvider>
+        <Routers>
+          <Router path="/" element={<Home />} />
+          <Router path="/edit" element={<Edit />} />
+        </Routers>
+      </RouterProvider>
+    </div>
+  );
 }
 
 export default App;
