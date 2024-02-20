413476afdba8598bfc2c88302c798837fcc40b0d
muse9312 <muse9312@naver.com>
2024-02-20 20:54:07 +0900

    project reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import "./styles/reset.scss";
-import "./styles/page.scss";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Edit from "./pages/Edit";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/edit" element={<Edit />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/components/List.jsx
+++
@@ -1,5 +0,0 @@
-const List = ({ children }) => {
-  return <ul className="wrapper-content__section-itembox">{children}</ul>;
-};
-
-export default List;
