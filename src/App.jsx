d78f414ee5802b1377a2f661c0193417ee0123b9
muse9312 <muse9312@naver.com>
2024-02-20 21:04:31 +0900

    fix : pages create & connection react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
